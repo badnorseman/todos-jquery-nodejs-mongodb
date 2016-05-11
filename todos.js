25f7322ea36cf64fa9801505b51aa11a4cd53c80
Dan Jensen <dj@fitbird.com>
2016-05-11 13:53:25 -0400

    update tests

--- todos.js
+++ todos.js
@@ -4,18 +4,19 @@ const todoSchema = mongoose.Schema({ todo: String })
 const Todo = mongoose.model('Todo', todoSchema)
 const Todos = require('./storage')
 
-exports.create = function(req, res) {
+exports.create = (req, res) => {
   if (!req.body || !req.body.name) return res.sendStatus(400)
   const name = req.body.name
   const todo = Todos.create(name)
   res.status(201).json(todo)
-  // Todo.save((err, todo) => {
-  //   if (err) return res.status(500).json({ err })
+  // const todo = { name }
+  // Todo.create(todo, (err, todo) => {
+  //   if (err || !todo) return res.status(500).json({ err })
   //   res.status(201).json({ todo })
   // })
 }
 
-exports.retrieveAll = function(req, res) {
+exports.retrieveAll = (req, res) => {
   const todos = Todos.retrieveAll()
   res.status(200).json(todos)
   // Todo.find((err, todos) => {
@@ -25,7 +26,7 @@ exports.retrieveAll = function(req, res) {
   // })
 }
 
-exports.update = function(req, res) {
+exports.update = (req, res) => {
   if (!req.params || !req.params.id || !req.body || !req.body.name) return res.sendStatus(400)
   const id = req.params.id
   const name = req.body.name
@@ -33,7 +34,7 @@ exports.update = function(req, res) {
   res.status(200).json(todo)
 }
 
-exports.delete = function(req, res) {
+exports.delete = (req, res) => {
   if (!req.params || !req.params.id) return res.sendStatus(400)
   const id = req.params.id
   const todo = Todos.delete(id)

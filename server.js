17a9d8981dfb756b65aa3e5af1fff0dc186592da
Dan Jensen <urbanvikingr@gmail.com>
2016-08-28 18:20:47 -0400

    refactoring app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const todos = require('./todos')
+const todos = require('./app/todos')
 require('./env')
 
 const app = express()

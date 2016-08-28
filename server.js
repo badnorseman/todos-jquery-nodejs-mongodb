feb130f7d85f70e67c1ced7c48e9393dd85dd294
Dan Jensen <urbanvikingr@gmail.com>
2016-08-28 19:04:30 -0400

    update server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser')
 const todos = require('./app/todos')
 require('./env')
 
+mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PSWD}@${process.env.MONGODB_URI}`)
+const db = mongoose.connection
+
+db.on('error', () => console.error('connection error'))
+db.once('open', () => console.error('connection success'))
+
 const app = express()
 app.use(bodyParser.json())
 app.use(express.static('public'))
@@ -14,13 +21,7 @@ app.post('/todos', (req, res) => todos.create(req, res))
 app.put('/todos/:id', (req, res) => todos.update(req, res))
 app.delete('/todos/:id', (req, res) => todos.delete(req, res))
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`listening on ${port}`))
-
-mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PSWD}@${process.env.MONGODB_URI}`)
-const db = mongoose.connection
-
-db.on('error', () => console.error('connection error'))
-db.once('open', () => console.error('connection success'))
 
 module.exports = app
